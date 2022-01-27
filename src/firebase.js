c834e6055b34b0207527c5da45294e48436577a5
nparashar150 <nparashar150@gmail.com>
2022-01-28 02:32:33 +0530

    Save User Email to Firestore

--- src/firebase.js
+++ src/firebase.js
@@ -5,15 +5,53 @@ import {
   sendPasswordResetEmail,
   signInWithPopup,
   GoogleAuthProvider,
-  signOut
+  signOut,
 } from "firebase/auth"
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  getDocs,
+  Timestamp,
+  collection,
+  query,
+  where,
+} from "firebase/firestore"
 
 const provider = new GoogleAuthProvider()
 const auth = getAuth()
+const db = getFirestore()
 
-export const createUserAccount = async (email, password) => {
+const saveUser = async (email, uid, name) => {
+  const checkIfUserExists = query(collection(db, "email"),where("user", "==", email))
+  const queryData = await getDocs(checkIfUserExists)
+  let flag = [];
+  queryData.forEach(doc => {
+    flag.push(doc.id)
+  })
+  if (flag.includes(email)) {
+    console.log("exists");
+  } else {
+    await setDoc(doc(db, "email", email), {
+      user: email,
+      uid,
+      name,
+      createdAt: Timestamp.fromDate(new Date()),
+    })
+    console.log("saved")
+  }
+}
+
+export const createUserAccount = async (
+  email,
+  password,
+  firstName,
+  lastName
+) => {
   try {
     const res = await createUserWithEmailAndPassword(auth, email, password)
+    const name = firstName + " " + lastName
+    saveUser(email, res.user.uid, name)
     console.log(res)
   } catch (err) {
     console.error(err)
@@ -42,6 +80,7 @@ export const loginWithGoogleAccount = async () => {
   try {
     const res = await signInWithPopup(auth, provider)
     console.log(res)
+    await saveUser(res.user.email, res.user.uid, res.user.displayName)
   } catch (err) {
     console.error(err)
   }
