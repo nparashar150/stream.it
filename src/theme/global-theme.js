8b60e646d08106199c7243f7cea29740c7ef7fc3
nparashar150 <nparashar150@gmail.com>
2022-01-28 00:00:22 +0530

    Create Global Theme

---
+++ src/theme/global-theme.js
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const lightTheme = {
+  background: '#FFF5F7',
+  font: '#16162C',
+  offline: '#16162C40',
+  online: '#16162Ca6',
+  userBorderColor: '#16162C40',
+};
+
+export const darkTheme = {
+  background: '#16162C',
+  font: '#FFFFFF',
+  offline: '#3ee0cf80',
+  online: '#40E0D0',
+  userBorderColor: '#ffffff40',
+};
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    transition: all 0.375 ease-in-out;
+  }
+  body {
+    background-color:  ${(props) => props.theme.background};
+    color: ${props => props.theme.font};
+  }
+`;
