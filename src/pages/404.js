566183179dd60b87584c12c95931a3a43138b1f2
nparashar150 <nparashar150@gmail.com>
2022-01-28 16:59:35 +0530

    Create 404 Page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,33 @@
-import * as React from "react"
-
+import React, { useEffect } from "react"
+import { navigate } from "gatsby"
+import {
+  NotFoundWrapper,
+  NotFoundHeading,
+  NotFoundInfo,
+} from "../components/404/NotFound"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const NotFoundPage = () => (
-  <Layout>
-    <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+const NotFoundPage = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/")
+    }, 2500)
+  }, [])
+
+  return (
+    <Layout>
+      <Seo title="404: Not found" />
+      <NotFoundWrapper className="container d-flex justify-content-center align-items-center">
+        <NotFoundHeading>404: Not Found</NotFoundHeading>
+        <NotFoundInfo>
+          You just hit a route that doesn&#39;t exist... the sadness.
+          <br />
+          Please wait, taking you back...
+        </NotFoundInfo>
+      </NotFoundWrapper>
+    </Layout>
+  )
+}
 
 export default NotFoundPage
