8a7ec6686fae946b6f53ceadc6589bc16517e772
nparashar150 <nparashar150@gmail.com>
2022-01-29 18:14:13 +0530

    Template Specific Movie Info

--- src/components/MovieList/MovieListMapper.js
+++ src/components/MovieList/MovieListMapper.js
@@ -5,40 +5,51 @@ import {
   MovieItemHeading,
   MovieItemInfo,
   MovieImageWrapper,
-} from "./MovieList"
+} from "./MovieListStyles"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { Link } from "gatsby"
 
 export default function MovieListMapper({ prismic }) {
   const bannerStyles = {
     overflow: "hidden",
-    objectFit: "cover"
+    objectFit: "cover",
   }
   return (
-      <MovieListWrapper className="container px-2">
-      {Object.keys(prismic).map((key) => {
-        const image = getImage(prismic[key].node.data.movie_banner.gatsbyImageData)
+    <MovieListWrapper className="container px-2">
+      {Object.keys(prismic).map(key => {
+        const image = getImage(
+          prismic[key].node.data.movie_banner.gatsbyImageData
+        )
         return (
-          <MovieItemWrapper key={key}>
-            <MovieImageWrapper>
-              <GatsbyImage
-                image={image}
-                formats={["auto", "webp", "avif"]}
-                alt={"image"}
-                style={bannerStyles}
-              />
-            </MovieImageWrapper>
-            <MovieItemHeading>{prismic[key].node.data.movie_title.text}</MovieItemHeading>
-            <div>
-              <MovieItemInfo releaseDate>{prismic[key].node.data.movie_release_date1.slice(0,4)}</MovieItemInfo>
-              <MovieItemInfo genre>
-                {prismic[key].node.data.movie_genre.text}
+          <Link key={key} to={`/details/${prismic[key].node.data.movie_title.text}`} >
+            <MovieItemWrapper>
+              <MovieImageWrapper>
+                <GatsbyImage
+                  image={image}
+                  formats={["auto", "webp", "avif"]}
+                  alt={"image"}
+                  style={bannerStyles}
+                />
+              </MovieImageWrapper>
+              <MovieItemHeading>
+                {prismic[key].node.data.movie_title.text}
+              </MovieItemHeading>
+              <div>
+                <MovieItemInfo releaseDate>
+                  {prismic[key].node.data.movie_release_date1.slice(0, 4)}
+                </MovieItemInfo>
+                <MovieItemInfo genre>
+                  {prismic[key].node.data.movie_genre.text}
+                </MovieItemInfo>
+                <MovieItemInfo rating>
+                  {"Rating: " + prismic[key].node.data.movie_rating}
+                </MovieItemInfo>
+              </div>
+              <MovieItemInfo description>
+                {prismic[key].node.data.movie_description.text}
               </MovieItemInfo>
-              <MovieItemInfo rating>
-                {"Rating: " + prismic[key].node.data.movie_rating}
-              </MovieItemInfo>
-            </div>
-            <MovieItemInfo description>{prismic[key].node.data.movie_description.text}</MovieItemInfo>
-          </MovieItemWrapper>
+            </MovieItemWrapper>
+          </Link>
         )
       })}
     </MovieListWrapper>
---
+++ src/templates/MovieInfo/MovieInfo.js
@@ -0,0 +1,96 @@
+import React, { useState, useEffect } from "react"
+import Layout from "../../components/layout"
+import {
+  MovieInfoWrapper,
+  MovieStory,
+  MovieHeading,
+  MovieInsightsWrapper,
+  MovieInsights,
+  MovieImage,
+  GoBack
+} from "./MovieInfoElements"
+import Seo from "../../components/seo"
+import { graphql, navigate } from "gatsby"
+import { getImage } from "gatsby-plugin-image"
+
+export default function MovieInfo({ data }) {
+  const [movieInfo, setMovieInfo] = useState({})
+  const imageStyles = {
+    overflow: "hidden",
+    objectFit: "cover",
+    marginLeft: "1rem",
+  }
+
+  useEffect(() => {
+    setMovieInfo(data.allPrismicPrefixNamanparashar.edges[0].node.data)
+  }, [data.allPrismicPrefixNamanparashar.edges])
+
+  const image = getImage(movieInfo?.movie_banner?.gatsbyImageData)
+
+  return (
+    <Layout>
+      <Seo title={movieInfo?.movie_title?.text} />
+      <MovieInfoWrapper className="container">
+        <GoBack onClick={() => navigate(-1)}>Go Back</GoBack>
+        <MovieImage
+          image={image}
+          formats={["auto", "webp", "avif"]}
+          alt={"image"}
+          style={imageStyles}
+        />
+        <MovieInsightsWrapper>
+          <MovieHeading>{movieInfo?.movie_title?.text}</MovieHeading>
+          <MovieInsights genre>{movieInfo?.movie_genre?.text}</MovieInsights>
+          <MovieInsights rating>
+            {"Rating: " + movieInfo?.movie_rating}
+          </MovieInsights>
+          <MovieInsights releaseDate>
+            {movieInfo?.movie_release_date1?.slice(0, 4)}
+          </MovieInsights>
+          <MovieInsights description>
+            {movieInfo?.movie_description?.text}
+          </MovieInsights>
+        </MovieInsightsWrapper>
+        <MovieStory>
+          <b>
+            Plot
+            <br />
+          </b>
+          {movieInfo?.movie_story?.text}
+        </MovieStory>
+      </MovieInfoWrapper>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query getSpecificMovie($name: String) {
+    allPrismicPrefixNamanparashar(
+      filter: { data: { movie_title: { text: { eq: $name } } } }
+    ) {
+      edges {
+        node {
+          data {
+            movie_banner {
+              gatsbyImageData(width: 220, height: 350, placeholder: BLURRED)
+            }
+            movie_description {
+              text
+            }
+            movie_genre {
+              text
+            }
+            movie_rating
+            movie_release_date1
+            movie_story {
+              text
+            }
+            movie_title {
+              text
+            }
+          }
+        }
+      }
+    }
+  }
+`
